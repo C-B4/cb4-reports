37df3ee177e47f0e3e12e3ddcaa660d10b74c8fa
Rafi Rainshtein <rafir@cb4.com>
2021-02-01 13:23:57 +0200

    Updated release to v1.0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cb4_reports_exporter',
-      version='1.0.2',
+      version='1.0.3',
       description='Export reports from CB4',
       author='CB4',
       author_email="support@cb4.com",
